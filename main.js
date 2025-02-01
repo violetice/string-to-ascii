e76b07884cfcb4d37bfbaeb728c594e685df1c27
Violet_Ice紫冰 <violetice@aliyun.com>
2025-02-01 20:46:19 +0800

    CharCode → CodePoint

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-export const stringToASCII = (str = '') => {
+export const stringToAscii = (str = '') => {
     let _str = ''
     for (let strElement of str) {
-        let char = `&#${strElement.charCodeAt(0)};`
+        let char = `&#${strElement.codePointAt(0)};`
         _str += char
     }
     return _str
+}
+
+export const asciiToString = (ascii = '') => {
+    let arr = ascii.split(';')
+    arr = arr.filter(v => v !== '')
+    let chars = arr.map(v => String.fromCodePoint(Number(v.replace('&#', ''))))
+    return chars.join('')
 }
--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-import {stringToASCII} from "./main.js";
+import {asciiToString, stringToAscii} from "./main.js";
 
-let str = stringToASCII('「小子，胆挺肥啊，大明星都敢敲诈。现在人赃俱获，洗洗屁股准备蹲苦窑吧！」')
+const STRING = '带头🤲的队🧧长过去拔下硬盘，走过来蹲下轻松的调侃道：「小子，胆挺肥啊，大明星都敢敲诈。现在人赃俱获，洗洗屁股准备蹲苦窑吧！」'
+
+let ascii = stringToAscii(STRING)
+console.log(ascii)
+
+let str = asciiToString(ascii)
 console.log(str)

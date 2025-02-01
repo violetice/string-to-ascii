ec656fd5d2ff63537caf22b68a19cb022a93b435
Violet_Ice紫冰 <violetice@aliyun.com>
2025-02-01 22:40:23 +0800

    test改为Hello World

--- test.js
+++ test.js
@@ -7,7 +7,7 @@
 
 import {asciiToString, stringToAscii} from "./main.js";
 
-const STRING = '带头🤲的队🧧长过去拔下硬盘，走过来蹲下轻松的调侃道：「小子，胆挺肥啊，大明星都敢敲诈。现在人赃俱获，洗洗屁股准备蹲苦窑吧！」'
+const STRING = 'Hello World'
 
 let ascii = stringToAscii(STRING)
 console.log(ascii)
